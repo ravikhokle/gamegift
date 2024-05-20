2d647341f328e7d7eb1b0127e2a716c822bc09b4
Ravi Khokle <131489761+ravikhokle@users.noreply.github.com>
2024-05-21 00:53:05 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<hr>
-    <footer class="footersection">
-        <p>2024 &copy; FruitBattlegrounds.cc - All Right Reserved.</p>
-    </footer>
-
-    <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="script.js"></script>
-</body>
